13a1483aa2163d5dc8556fe31af07e332a9dbc83
Paweł Stawicki <pawelstawicki@gmail.com>
2012-02-05 23:03:05 +0100

    Form working

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,33 @@
 package controllers
 
-import play.api._
+import play.api.data._
 import play.api.mvc._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  val inputForm = Form(
+    of(
+      "inputString" -> text,
+      "arg1" -> number,
+      "arg2" -> number,
+      "arg3" -> number
+    )
+  )
+
+  def input = Action {
+    Ok(views.html.input(inputForm))
+  }
+
+  def format = Action { implicit request =>
+    val input = inputForm.bindFromRequest().get._1
+    val arg1: java.lang.Long = inputForm.bindFromRequest().get._2
+    val arg2: java.lang.Long = inputForm.bindFromRequest().get._3
+    val arg3: java.lang.Long = inputForm.bindFromRequest().get._4
+    
+    val f = java.lang.String.format(input, arg1, arg2, arg3);
+    
+    Ok(views.html.format(f))
+//    Ok(views.html.format("Request: " + request.queryString.get("inputString").flatMap(_.headOption)))
   }
   
 }
